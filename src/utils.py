5ee928abacbe1f6e8cd2b3c10b59d690ea440e49
2807vaibhav <vaibhavbhajipale1@gmail.com>
2023-06-06 19:25:14 +0530

    preddiction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,8 @@
 import sys
 import os
-import dill
-import os
-import sys
-
 import numpy as np 
 import pandas as pd
 import dill
-import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -63,3 +58,4 @@ def load_object(file_path):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
